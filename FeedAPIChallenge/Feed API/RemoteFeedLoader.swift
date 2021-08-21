ca91a9e290699868b5d7434665e9873a3d7aacef
Klaudius Ivan <v-klaudius.ivan@anteraja.id>
2021-08-21 21:35:27 +0700

    make object to decode the data and return with expected object, and use response to check the status code only accept 200 else return error

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,30 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(Error.invalidData))
+			case .success(let (data, response)):
+				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success(root.items.map({ $0.item })))
+				} else {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private struct Root: Decodable {
+	let items: [Item]
+}
+
+private struct Item: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+
+	var item: FeedImage {
+		return FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
