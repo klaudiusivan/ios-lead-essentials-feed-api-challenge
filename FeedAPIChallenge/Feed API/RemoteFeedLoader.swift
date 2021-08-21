1384f1c5378a06e42a348bfb1b2ca8c774e6ffbd
Klaudius Ivan <v-klaudius.ivan@anteraja.id>
2021-08-21 21:48:38 +0700

    make Feed Image Mapper to reduce the code in load function, and make it easier to read, pass the sixth test

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,8 +22,8 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { result in
 			switch result {
 			case .success(let (data, response)):
-				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.items.map({ $0.item })))
+				if let items = try? FeedImageMapper.map(data, response) {
+					completion(.success(items))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
@@ -34,17 +34,34 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 }
 
-private struct Root: Decodable {
-	let items: [Item]
-}
+internal struct FeedImageMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+	}
 
-private struct Item: Decodable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let url: URL
+	private struct Item: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
 
-	var item: FeedImage {
-		return FeedImage(id: id, description: description, location: location, url: url)
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+
+		var item: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == 200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+
+		return try JSONDecoder().decode(Root.self, from: data).items.map({ $0.item })
 	}
 }
